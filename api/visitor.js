222418d6d519a9b79f0b76a2dc22e6f1bdbcebf7
Muksin Alfalah <49297279+muksin-alfalah@users.noreply.github.com>
2025-10-13 15:26:51 +0700

    Update visitor.js

--- api/visitor.js
+++ api/visitor.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const url = 'https://api.counterapi.dev/peta-data-pemilih-tebo/global-visitors?increment=true';
+  const url = 'https://api.countapi.xyz/update/peta-data-pemilih-tebo/global-visitors/?amount=1';
 
   try {
     const response = await fetch(url);
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.status(200).json(data);
   } catch (error) {
-    console.error(error);
+    console.error('Error di server:', error);
     res.status(500).json({ error: 'Gagal mengambil data pengunjung' });
   }
 }
