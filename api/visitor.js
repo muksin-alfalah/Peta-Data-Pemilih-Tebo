07c51495daef8abf93154c9048ee6cc087814357
Muksin Alfalah <49297279+muksin-alfalah@users.noreply.github.com>
2025-10-13 15:24:57 +0700

    Update visitor.js

--- api/visitor.js
+++ api/visitor.js
@@ -8,6 +8,7 @@ export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.status(200).json(data);
   } catch (error) {
+    console.error(error);
     res.status(500).json({ error: 'Gagal mengambil data pengunjung' });
   }
 }
